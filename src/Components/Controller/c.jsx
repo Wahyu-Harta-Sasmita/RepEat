97140a203382c712d87eb4b7ad0c8b875d57000c
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2024-10-27 20:39:06 +0800

    Add project files

---
+++ src/Components/Controller/c.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const c = () => {
+  return (
+    <div>c</div>
+  )
+}
---
+++ src/Pages/Store ~ Wahyu/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <>
+        <p className="text-3xl font-bold">Dashboard</p>
+    </>
+  )
+}
+
+export default Dashboard
