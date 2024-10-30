e4ffd8a29f67719ffb7a8fdc89d3b10e435c87f3
Wahyu-Harta-Sasmita <wahyusasmitaharta561@gmail.com>
2024-10-30 18:52:51 +0800

    wahyu update3

--- src/Components/Wahyu/statsCard.jsx
+++ src/Components/Wahyu/statsCard.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-function StatsCard({ title, amount, percentage }) {
+function StatsCard({ title, amount }) {
   return (
-    <Link to="/seller/products">
       <div className="bg-white p-4 rounded-lg shadow-md">
         <h3 className="text-gray-500">{title}</h3>
         <div className="flex justify-between items-center">
           <p className="text-2xl font-bold">{amount}</p>
-          <span className="text-green-500">{percentage}</span>
         </div>
       </div>
-    </Link>
   );
 }
 
--- src/Pages/Store ~ Wahyu/Dashboard.jsx
+++ src/Pages/Store ~ Wahyu/Dashboard.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Sidebar from '../../Components/Wahyu/sidebar'
 import Header from '../../Components/Wahyu/header'
 import StatsCard from '../../Components/Wahyu/statsCard'
@@ -11,10 +12,9 @@ const Dashboard = () => {
       <div className="flex-1 p-6 space-y-6">
         <Header />
         <div className="grid grid-cols-3 gap-4">
-          <StatsCard title="Pendapatan Kamu" amount="Rp 1,300,000" percentage="15%" />
-          <StatsCard title="Total Pesanan" amount="31,500" percentage="25%" />
-          <StatsCard title="Total Donasi" amount="Rp 300,000" percentage="35%" />
-          <StatsCard title="Total Produk" amount="247" percentage="5%" />
+          <StatsCard title="Pendapatan Kamu" amount="Rp 1,300,000"/>
+          <StatsCard title="Total Pesanan" amount="31,500" />
+          <Link to="/seller/Products"><StatsCard title="Total Produk" amount="247"/></Link>
         </div>
         <OrdersTable />
       </div>
